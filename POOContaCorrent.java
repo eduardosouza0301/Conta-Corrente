cb3fdb8d78e69639720e96f4690b2b8fa528dcd6
Eduardo Souza de Oliveira <eduardosouza0301@gmail.com>
2025-05-26 19:42:06 -0300

    Publicado
    Código Java do Básico ao Avançado

---
+++ POOContaCorrent.java
@@ -0,0 +1,28 @@
+public class POOContaCorrent {
+
+    public static void main(String[] args) {
+
+        ContaCorrent Eduardo = new ContaCorrent();
+        
+        Eduardo.depositar(200);
+
+        Eduardo.sacar(50);
+
+        Eduardo.exibirSaldo(0);
+
+        ContaCorrent Vania = new ContaCorrent();
+
+        Vania.depositar(400);
+
+        Vania.sacar(150);
+
+        Vania.exibirSaldo(0);
+
+       
+
+        
+
+
+    }
+    
+}
